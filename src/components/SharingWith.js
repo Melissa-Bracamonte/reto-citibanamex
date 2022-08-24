a293d287beb3d7756992bc963b8790f99df363e9
Melissa Bracamonte <melissabracamonte30@gmail.com>
2022-08-24 14:14:40 -0500

    crear endpoint para guardar el gasto que el usuario desea compartir

--- src/components/SharingWith.js
+++ src/components/SharingWith.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { ModalAddAccount } from "./ModalAddAccount";
 import "../styles/abstracts/sharingwith.scss";
 import cardDebito from "../img/cardBank.png";
-import { FaDollarSign } from 'react-icons/fa';
-import { BsFillPeopleFill } from 'react-icons/bs';
+import { FaDollarSign } from "react-icons/fa";
+import { BsFillPeopleFill } from "react-icons/bs";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 
@@ -13,6 +13,7 @@ const SharingWith = () => {
     cards: [],
   });
   const [objPopup, setPopup] = useState({ visibility: false });
+  const [totalAmount, setTotalAmount] = useState([]);
   const navigate = useNavigate();
 
   const getAllClients = () => {
@@ -27,29 +28,45 @@ const SharingWith = () => {
   useEffect(() => {
     getAllClients();
   }, []);
-  
+
+  const totalToSplit = () => {
+    fetch("https://6305077894b8c58fd72a83cd.mockapi.io/expenses")
+      .then((response) => response.json())
+      .then((expenses) => setTotalAmount(expenses));
+  };
+  console.log(totalAmount);
+
+  useEffect(() => {
+    totalToSplit();
+  }, []);
+
   const handleChange = (e) => {
     const checked = e.target.checked;
     const value = e.target.id;
-   
+
     const { cards } = data;
 
     if (checked) {
       setData({
-        cards: [...cards, value ]
+        cards: [...cards, value],
       });
     } else {
       setData({
-        cards: cards.filter((e) => e !== value)
+        cards: cards.filter((e) => e !== value),
       });
     }
   };
-  const totalCheckList=()=>{
-    const total = data.cards.length;
-    console.log(total)
-  } 
 
+  let total;
+  const totalCheckList = () => {
+    // const total = data.cards.length;
+    total = data.cards.length;
+    console.log(total);
+  };
 
+  const SplitExpenses = () => {
+
+  };
 
   const onAdd = () => {
     let popupProduct = {};
@@ -72,7 +89,7 @@ const SharingWith = () => {
         visible={objPopup.visibility}
         attrProduct={objPopup.popupProduct}
       />
-<AiOutlineArrowLeft className="arrowBack" onClick={redirectBack} />
+      <AiOutlineArrowLeft className="arrowBack" onClick={redirectBack} />
       <section className="containerClients">
         <p className="h1 d-flex justify-content-center tittle-sharing">
           2. Compartir con:
@@ -104,7 +121,6 @@ const SharingWith = () => {
                           type="checkbox"
                           id={item.id}
                           onChange={handleChange}
-                      
                         />
                       </div>
                     </section>
@@ -115,21 +131,35 @@ const SharingWith = () => {
           })}
         </section>
 
-        <div className='container-btn'>
-          <button className='btn-newperson' onClick={onAdd}><FaDollarSign className="icon-dollar" /> <p className="txt-btn">Nueva Cuenta</p></button>
-          <button className='btn-dg'> <BsFillPeopleFill /> <p className="txt-btn" onClick={totalCheckList}>Dividir Gastos</p></button>
+        <div className="container-btn">
+          <button className="btn-newperson" onClick={onAdd}>
+            <FaDollarSign className="icon-dollar" />{" "}
+            <p className="txt-btn">Nueva Cuenta</p>
+          </button>
+          <button className="btn-dg">
+            {" "}
+            <BsFillPeopleFill />{" "}
+            <p className="txt-btn" onClick={totalCheckList}>
+              Dividir Gastos
+            </p>
+          </button>
         </div>
 
         <div className="list-result">
-          <h5 className="txt-res">Melissa Bracamonte <span> $400.00</span></h5>
-          <h5 className="txt-res">Daniela Miñon <span> $400.00</span></h5>
-          <h5 className="txt-res">Melissa Bracamonte <span> $400.00</span></h5>
+          <h5 className="txt-res">
+            Melissa Bracamonte <span> $400.00</span>
+          </h5>
+          <h5 className="txt-res">
+            Daniela Miñon <span> $400.00</span>
+          </h5>
+          <h5 className="txt-res">
+            Melissa Bracamonte <span> $400.00</span>
+          </h5>
         </div>
 
         <div className="container-return">
           <button className="return">Volver al inicio</button>
         </div>
-
       </section>
     </>
   );
