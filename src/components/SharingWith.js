b86c3d0b8d2d3f60b7228435768d69b3c955fe87
veroguzrob72 <vgrdesd@gmail.com>
2022-08-24 14:08:55 -0500

    Se modifican estilos a la 4a vista

--- src/components/SharingWith.js
+++ src/components/SharingWith.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { ModalAddAccount } from "./ModalAddAccount";
 import "../styles/abstracts/sharingwith.scss";
 import cardDebito from "../img/cardBank.png";
 import { FaDollarSign } from 'react-icons/fa';
 import { BsFillPeopleFill } from 'react-icons/bs';
+import { MdQrCode } from 'react-icons/md';
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { BiQuestionMark } from 'react-icons/bi';
+
 
 const SharingWith = () => {
   const [clients, setClients] = useState([]);
@@ -75,6 +80,14 @@ const SharingWith = () => {
     setPopup({ visibility: false });
   };
 
+  const navigate = useNavigate();
+  const redirectBack = () => {
+    navigate("/movimientos");
+  };
+  const redirectInformation = () => {
+    navigate("/informacion");
+  };
+
   return (
     <>
       <ModalAddAccount
@@ -82,6 +95,9 @@ const SharingWith = () => {
         visible={objPopup.visibility}
         attrProduct={objPopup.popupProduct}
       />
+      <AiOutlineArrowLeft className="arrowBack" onClick={redirectBack} />
+      <BiQuestionMark className="question" onClick={redirectInformation} />
+
       <section className="containerClients">
         <p className="h1 d-flex justify-content-center tittle-sharing">
           2. Compartir con:
@@ -91,22 +107,24 @@ const SharingWith = () => {
             return (
               <div key={item.id}>
                 <div className="card baseSimple">
-                  <div className="card-body d-flex flex-row justify-content-between  ">
-                    <section className="mr-auto p-2">
+                  <div className="card-body d-flex flex-row justify-content-between p-1">
+                    <section className="card-inf mr-auto p-0">
                       <img
                         className="card-imgDebito"
                         src={cardDebito}
                         alt="img tarjeta"
                       />
-                      <h5 className="card-title double">
-                        {item.name} <span />{" "}
-                        {maskifyCardsContact(item.cardNumber)}{" "}
-                      </h5>
-                      <h6 className="card-subtitle mb-2 text-muted double-upper">
-                        {item.bank} <span /> {item.name}{" "}
-                      </h6>
+                      <div className="card-data">
+                        <h5 className="card-title  double">
+                          {item.name} <br /> {" "}
+                          {maskifyCardsContact(item.cardNumber)}{" "}
+                        </h5>
+                        <h6 className="card-subtitle mb-1 text-muted double-upper">
+                          {item.bank} /<span /> {item.name}{" "}
+                        </h6>
+                      </div>
                     </section>
-                    <section className="p-2">
+                    <section className="p-2 ">
                       <div className="form-check form-check-reverse check">
                         <input
                           className="form-check-input"
@@ -136,14 +154,44 @@ const SharingWith = () => {
           <button className='btn-dg'> <BsFillPeopleFill /> <p className="txt-btn">Dividir Gastos</p></button>
         </div>
 
-        <div className="list-result">
-          <h5 className="txt-res">Melissa Bracamonte <span> $400.00</span></h5>
-          <h5 className="txt-res">Daniela Miñon <span> $400.00</span></h5>
-          <h5 className="txt-res">Melissa Bracamonte <span> $400.00</span></h5>
+        <div className="card result">
+          <div className="card-body d-flex flex-row justify-content-between p-1">
+            <section className="mr-auto p-1">
+              <h5 className="card-title">Daniela Miñon</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                25/07
+              </h6>
+            </section>
+            <section className="p-1 d-flex flex-column ">
+              <p className="card-text txt-res">$400.00</p>
+            </section>
+          </div>
+          <div className="card-body d-flex flex-row justify-content-between p-1">
+            <section className="mr-auto p-1">
+              <h5 className="card-title">Melissa Bracamonte</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                25/07
+              </h6>
+            </section>
+            <section className="p-1 d-flex flex-column ">
+              <p className="card-text txt-res">$400.00</p>
+            </section>
+          </div>
+          <div className="card-body d-flex flex-row justify-content-between p-1">
+            <section className="mr-auto p-1">
+              <h5 className="card-title">Total</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+              </h6>
+            </section>
+            <section className="p-1 d-flex flex-column ">
+              <p className="card-text txt-res">$800.00</p>
+            </section>
+          </div>
         </div>
 
+
         <div className="container-return">
-          <button className="return">Volver al inicio</button>
+          <button className="return"> <MdQrCode /> <p className="txt-btn">Generar CoDi</p></button>
         </div>
 
       </section>
